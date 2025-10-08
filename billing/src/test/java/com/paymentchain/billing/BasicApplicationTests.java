8291773df5433914b6418d0c783eff9c613b8665
yaselramos <yaselramos@gmail.com>
2025-10-08 09:48:38 -0300

    change get by orElseTrow

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -10,7 +10,7 @@ public class BasicApplicationTests  {
 
     @Test
     void contextLoads() {
-       String message = "Default Message Cambio 11";
+       String message = "Default Message Cambio 12";
         assertNotNull(message);
     }
 }
